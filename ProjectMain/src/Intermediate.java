a8995ecaac9662ddfecf8a6e735d14c4baad039c
Glenn Paul Sumido <glennpaul@users.noreply.github.com>
2016-10-17 10:20:43 -0400

    Updated for Iteration 3
    Added prompt for type of error simulation and added if structure that will simulate those errors. Still need checks for what type of error to simulate aka not done yet

--- ProjectMain/src/Intermediate.java
+++ ProjectMain/src/Intermediate.java
@@ -17,6 +17,24 @@ public class Intermediate implements Runnable{
       
     public void run()
     {
+        System.out.print("Enter (1) for normal operation, enter (1) to lose a packet, enter (3) delay a packet, or enter (4) to duplicate a packet:");
+        int choice = scan.nextInt();		
+
+        if ( choice == 1){
+          //do nothing
+        } else if ( choice == 2 ){
+          System.out.print("Choose an integer number for which packet to lose:");
+          int chosenPacket = scan.nextInt();
+        } else if ( choice == 3 ){
+          System.out.print("Choose an integer number for which packet to delay:");
+          int chosenPacket = scan.nextInt();
+          System.out.print("Choose an integer number for how many milliseconds to delay:");
+          int delay = scan.nextInt();
+        } else if ( choice == 4) {
+          System.out.print("Choose an integer number for which packet to duplicate:");
+          int chosenPacket = scan.nextInt();	
+        }
+      
         while(true){
             try {
                 addThread();
@@ -31,36 +49,45 @@ public class Intermediate implements Runnable{
                   
                 //Process the received datagram.
                 printInfo(forwardingPacket);
+                
+              
+                /* insert statement to check for client sent ack or client sent data or request and chosen packet*/
+                if (true) {
                   
-                // Forward packet to server
-                forwardingPacket = new DatagramPacket(forwardingPacket.getData(), forwardingPacket.getLength(), InetAddress.getLocalHost(), 69);
+                  // Forward packet to server
+                  forwardingPacket = new DatagramPacket(forwardingPacket.getData(), forwardingPacket.getLength(), InetAddress.getLocalHost(), 69);
+
+                  //print out packet sent
+                  System.out.println("Intermediate: Packet sending:");
+                  System.out.println("String: " + new String(forwardingPacket.getData(),0,forwardingPacket.getLength()));
+                  System.out.println("Bytes: " + forwardingPacket.getData());       
+
+                  sendReceiveSocket = new DatagramSocket();
+                  sendReceiveSocket.send(forwardingPacket);
+
+                  // Receive response from server
+                  data = new byte[100];
+                  forwardingPacket = new DatagramPacket(data, data.length);
+
+                  sendReceiveSocket.receive(forwardingPacket);
+
+                  //print information
+                  printInfo(forwardingPacket);
                   
-                //print out packet sent
-                System.out.println("Intermediate: Packet sending:");
-                System.out.println("String: " + new String(forwardingPacket.getData(),0,forwardingPacket.getLength()));
-                System.out.println("Bytes: " + forwardingPacket.getData());       
- 
-                sendReceiveSocket = new DatagramSocket();
-                sendReceiveSocket.send(forwardingPacket);
+                  /* insert statement to check for server sent ack or server sent data and chosen packet*/
+	                if (true) {
+                    // Forward response to client
+                    forwardingPacket = new DatagramPacket(forwardingPacket.getData(), forwardingPacket.getLength(), clientAddress, clientPort);
+                    sendReceiveSocket.send(forwardingPacket);
+
+                    //print out sent datagram
+                    System.out.println("Intermediate: Packet sending:");
+                    System.out.println("String: " + new String(forwardingPacket.getData(),0,forwardingPacket.getLength()));
+                    System.out.println("Bytes: " + forwardingPacket.getData());
+                  }
                   
-                // Receive response from server
-                data = new byte[100];
-                forwardingPacket = new DatagramPacket(data, data.length);
-                 
-                sendReceiveSocket.receive(forwardingPacket);
-                  
-                //print information
-                printInfo(forwardingPacket);
-      
-                // Forward response to client
-                forwardingPacket = new DatagramPacket(forwardingPacket.getData(), forwardingPacket.getLength(), clientAddress, clientPort);
-                sendReceiveSocket.send(forwardingPacket);
-                  
-                //print out sent datagram
-                System.out.println("Intermediate: Packet sending:");
-                System.out.println("String: " + new String(forwardingPacket.getData(),0,forwardingPacket.getLength()));
-                System.out.println("Bytes: " + forwardingPacket.getData());
-                 
+                }
+              
                 removeThread();
                 stop(sendReceiveSocket);
                   
