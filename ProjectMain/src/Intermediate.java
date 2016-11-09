439ccfd10cc0ad44d14cc3e5ff61a645be2b4f1e
Glenn Paul Sumido <glennpaul@users.noreply.github.com>
2016-11-09 12:02:52 -0500

    Fully working for all iteration 3 errors

--- ProjectMain/src/Intermediate.java
+++ ProjectMain/src/Intermediate.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class Intermediate implements Runnable {
 
-    DatagramSocket receiveSocket, sendReceiveSocket;
-    //TODO: intermediate was supposed to go along side the client. Since the client is single threaded what is the use for this variable here? This variable is never used for any condition checking!
-    //DONE: i'm not the one that put it there so you may have to talk to whoever did
-    int threads = 0;//number of current threads
-    String type = null;
+    DatagramSocket receiveSocket, sendReceiveSocket;int threads = 0;//number of current threads
 
     public Intermediate() {
         try {
@@ -19,28 +15,26 @@ public class Intermediate implements Runnable {
     }
 
     public void run() {
-        int chosenPacket = 0;
-        String typeChosen = "";
-        String side = "";
+        String typeChosen = null;
+        String side = null;
+        String type = null;
         int delay = 0;
         int choice = 0;
         int clientPort;
         int serverPort = 2269;
-        Boolean verbose;
-
-        //TODO: maybe we need different packet numbers based on ack or data type
-        //DONE: shouldn't it be the same because there will be for example an ack #3 packet in response to data #3 packet?
         int packetNo = 0;
+        int chosenPacket = 0;
+		int servLength = 0;
+        Boolean verbose = false;
 
         Scanner scan = new Scanner(System.in);
         
-        System.out.print("Enter (0) for quiet mode or (1) for verbose:");
+        System.out.print("Enter (0) for quiet mode or (otherwise) for verbose:");
         if (scan.nextInt() == 0) {
         	verbose = false;
         } else {
         	verbose = true;
-        }
-        
+        }        
         
         while (choice < 1 || choice > 4) {
             System.out.print("Enter (1) for normal operation, enter (2) to lose a packet, enter (3) delay a packet, or enter (4) to duplicate a packet:");
@@ -54,7 +48,7 @@ public class Intermediate implements Runnable {
 	        chosenPacket = scan.nextInt();
 	        System.out.print("Choose type of packet to lose(request,ack, or data):");
 	        typeChosen = scan.next();
-	        System.out.print("Choose to implement error for server (type 0) or client (type 1):");
+	        System.out.print("Choose side to implement(client or server)");
 	        side = scan.next();
         } else if (choice == 3) {
             System.out.print("Choose an integer number for which packet to delay:");
@@ -63,7 +57,7 @@ public class Intermediate implements Runnable {
             delay = scan.nextInt();
             System.out.print("Choose type of packet to delay(request,ack, or data):");
             typeChosen = scan.next();
-            System.out.print("Choose to implement error for server (type 1) or client (type 2):");
+	        System.out.print("Choose side to implement(client or server)");
             side = scan.next();
         } else { // if (choice == 4)
             System.out.print("Choose an integer number for which packet to duplicate:");
@@ -72,7 +66,7 @@ public class Intermediate implements Runnable {
             //delay = scan.nextInt();
             System.out.print("Choose type of packet to duplicate(request,ack, or data):");
             typeChosen = scan.next();
-            System.out.print("Choose to implement error for server or client:");
+	        System.out.print("Choose side to implement(client or server)");
             side = scan.next();
         }
         scan.close();
@@ -97,16 +91,16 @@ public class Intermediate implements Runnable {
                 }
                 if (forwardingPacket.getData()[1] == 1 || forwardingPacket.getData()[1] == 2) {
                     type = "request";
-                    packetNo = forwardingPacket.getData()[3];
+                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
                 } else if (forwardingPacket.getData()[1] == 3) {
                     type = "data";
-                    packetNo = forwardingPacket.getData()[3];
+                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
                 } else if (forwardingPacket.getData()[1] == 4) {
                     type = "ack";
-                    packetNo = forwardingPacket.getData()[3];
+                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
                 } else if (forwardingPacket.getData()[1] == 5) {
                     type = "error";
-                    packetNo = forwardingPacket.getData()[3];
+                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
                 }
                 
                 //make packetNo unsigned? not sure how
@@ -114,13 +108,11 @@ public class Intermediate implements Runnable {
                 
               
                 /* insert statement to check for client sent ack or client sent data or request and chosen packet*/
-                //TODO: looks like: if user chooses side 2 (server) this if condition is not satisfied and the first packet (from client to server) is never delivered. Make sure if the packets don't match user specified criteria, the packet forwarding is working fine!
                 if (!(choice == 2 && type.equals(typeChosen)  && packetNo == chosenPacket && side.equals("client"))) {
 
                     if (choice == 3 && type.equals(typeChosen) && packetNo == chosenPacket && side.equals("client")) {
                         Thread.sleep(delay);
-                    }
-					
+                    }					
                     
 					// Forward packet to server
 					forwardingPacket = new DatagramPacket(forwardingPacket.getData(), forwardingPacket.getLength(), InetAddress.getLocalHost(), serverPort);
@@ -129,12 +121,14 @@ public class Intermediate implements Runnable {
 					if (verbose) {
 						printInfo(forwardingPacket);
 					}
-	
+					
+					//create sending socket and send packet
 					sendReceiveSocket = new DatagramSocket();
 					sendReceiveSocket.send(forwardingPacket);
 					
 					//if chose to duplicate this particular packet, send again
-					if (choice == 4 && type == typeChosen && packetNo == chosenPacket && side.equals("client")) {
+					if (choice == 4 && type.equals(typeChosen) && packetNo == chosenPacket && side.equals("client")) {
+						System.out.println("\nSending out duplicate packet\n");
 						sendReceiveSocket.send(forwardingPacket);
 				    }
 	
@@ -148,30 +142,30 @@ public class Intermediate implements Runnable {
 					if (verbose) {
 						printInfo(forwardingPacket);
 					}
-					int servLength = 0;
+					
 					//check for type again
 					if (forwardingPacket.getData()[1] == 1 || forwardingPacket.getData()[1] == 2) {
 	                    type = "request";
 	                    servLength = 516;
-	                    packetNo = forwardingPacket.getData()[3];
+	                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
 	                } else if (forwardingPacket.getData()[1] == 3) {
 	                    type = "data";
 	                    servLength = 516;	                    
-	                    packetNo = forwardingPacket.getData()[3];
+	                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
 	                } else if (forwardingPacket.getData()[1] == 4) {
 	                    type = "ack";
 	                    servLength = 4;
-	                    packetNo = forwardingPacket.getData()[3];
+	                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
 	                } else if (forwardingPacket.getData()[1] == 5) {
 	                    type = "error";
 	                    servLength = 4;
-	                    packetNo = forwardingPacket.getData()[3];
+	                    packetNo = forwardingPacket.getData()[3]+forwardingPacket.getData()[4];
 	                }
 	
 					/* insert statement to check for server sent ack or server sent data and chosen packet*/
 					if (!(choice == 2 && type.equals(typeChosen) && packetNo == chosenPacket && side.equals("server"))) {
 	
-					    if (choice == 3 && type == typeChosen && packetNo == chosenPacket && side.equals("server") && packetNo == chosenPacket) {
+					    if (choice == 3 && type.equals(typeChosen) && packetNo == chosenPacket && side.equals("server")) {
 	                        Thread.sleep(delay);
 					    }
 					    
@@ -180,7 +174,8 @@ public class Intermediate implements Runnable {
 					    receiveSocket.send(forwarding2Packet);
 					    
 					    //if choice is to duplicate this particular packet, send again
-					    if (choice == 4 && type == typeChosen && packetNo == chosenPacket && side.equals("server")) {
+					    if (choice == 4 && type.equals(typeChosen) && packetNo == chosenPacket && side.equals("server")) {
+							System.out.println("\nSending out duplicate packet\n");
 					    	sendReceiveSocket.send(forwarding2Packet);
 					    }
 					    
@@ -205,14 +200,13 @@ public class Intermediate implements Runnable {
     public void printInfo(DatagramPacket x) {
 
         // Process the received datagram.
-        System.out.println("\n:");
-        System.out.println("Host: " + x.getAddress() + " To port: " + x.getPort());
+        System.out.println("\nHost: " + x.getAddress() + " port: " + x.getPort());
         System.out.println("Length: " + x.getLength());
         // Form a String from the byte array.
         int block = x.getData()[3];
-        System.out.println("Block Number: " + block + "\n");
+        System.out.println("Block Number: " + block);
         System.out.println("Containing " + new String(x.getData(),0,(x.getData()).length));
-        System.out.println("Information in byte form: " + Arrays.toString(x.getData()) + "\n");
+        System.out.println("Information in byte form: " + Arrays.toString(x.getData()));
 
     }
 
