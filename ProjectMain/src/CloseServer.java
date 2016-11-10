ac613b71f6808d74759f6b806b64c94320561a76
Glenn Paul Sumido <glennpaul@users.noreply.github.com>
2016-11-10 15:19:39 -0500

    added proper close server

--- ProjectMain/src/CloseServer.java
+++ ProjectMain/src/CloseServer.java
@@ -1,20 +1,18 @@
-//Darshpreet Grewal
-//100972449
- 
 import java.util.Scanner;
-import java.io.File;
   
 //class used to close server 
 public class CloseServer implements Runnable {
       
     private Scanner in = new Scanner(System.in);
     private Server EndServer;
+    private Boolean done;
       
     public CloseServer(Server x){
         EndServer = x;
     }
       
     public void run(){
+    	done = false;
         while(true){
             System.out.println("To close server, please enter 'close'.");
             getTextInfo(in.next());
@@ -23,10 +21,17 @@ public class CloseServer implements Runnable {
       
     private void getTextInfo(String inputData){
         if (inputData.equals("close")){
-            EndServer.holder = false;
-            EndServer.shutdownServer();
-            System.exit(1);
+        	while (true) {
+        		if (EndServer.getThreads() == 0) {
+        			EndServer.shutdownServer();
+    	            System.exit(1);
+        		}
+        		try {
+        			Thread.sleep(500);
+        		} catch (InterruptedException e) {
+        			
+        		}
+        	}        		
         }
     }
-      
 }
