2e8876b27dba0a6e29844b78e2f1089b67f129ad
Glenn Paul Sumido <glennpaul@users.noreply.github.com>
2016-11-21 12:33:03 -0500

    fixed some error4 errors

--- ProjectMain/src/AssistantC.java
+++ ProjectMain/src/AssistantC.java
@@ -1,139 +1,170 @@
-import java.util.Arrays;
 import java.net.DatagramPacket;
-import java.net.SocketException;
-import java.net.DatagramSocket;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class AssistantC extends TransferHub implements Runnable{
-    
-    DatagramSocket SendSocket;
-//    byte [] fileinfo = new byte [512];
-    int pnum;
-    Server listener;
-    
-    private DatagramPacket reqPacket;
-  
-     
-    public AssistantC(int Port, DatagramPacket packet, Server x){
-        try {
-        	reqPacket = packet;
-        	listener = x;
-            SendSocket = new DatagramSocket();
-//            fileinfo = packet.getData();
-            pnum = Port;
-            IPAddress = x.IPAddress;
-        }catch(SocketException se){
-            se.printStackTrace();
-            System.exit(1);
-        }
+public class Utils {
+
+    public static final boolean SEND = true;
+    public static final boolean RECEIVE = false;
+
+    public static final byte RRQ = 1;
+    public static final byte WRQ = 2;
+    public static final byte DATA = 3;
+    public static final byte ACK = 4;
+    public static final byte ERR = 5;
+    public static final byte REQ = 10;
+
+    public static final Map<Byte, String> upCodeTypeMap = new HashMap<>();
+    static {
+        upCodeTypeMap.put(DATA, "DATA");
+        upCodeTypeMap.put(ACK, "ACK");
+        upCodeTypeMap.put(ERR, "ERR");
+        upCodeTypeMap.put(REQ, "RRQ/WRQ");
+        upCodeTypeMap.put(RRQ, "RRQ");
+        upCodeTypeMap.put(WRQ, "WRQ");
     }
- 
-     
+
+    public static int getBlockNo(DatagramPacket packet) {
+    	if (packet.getLength() < 4)
+    		return 0;
+        return ((packet.getData()[2] & 0xFF) * 256) + (packet.getData()[3] & 0xFF);
+    }
+
+    public static int getBlockNo(byte[] packet) {
+        int MSB;
+        int LSB;
+        if(packet.length > 4) {
+            MSB = 2;
+            LSB = 3;
+        } else {
+            MSB = packet.length - 2;
+            LSB = packet.length - 1;
+        }
+
+        return ((packet[MSB] & 0xFF) * 256) + (packet[LSB] & 0xFF);
+    }
+
+    public static void printInfo(DatagramPacket packet, boolean sendReceive) {
+        int packetLength = packet.getLength();
+
+        // Process the received datagram.
+        System.out.println("\nHost ID: " + packet.getAddress());
+        if(sendReceive == SEND) {
+            System.out.println("Sent on port number: " + packet.getPort());
+        } else {
+            System.out.println("Received on port number: " + packet.getPort());
+        }
+        System.out.println("Length of the packet: " + packetLength);
+        // Form a String from the byte array.
+        int block = getBlockNo(packet);
+        byte packType = packet.getData()[1];
+        System.out.printf("Type: %s Packet\n", upCodeTypeMap.get(packType));
+
+        System.out.println("Block Number: " + block);
+        System.out.println("Containing " + new String(packet.getData(),0,packetLength));
+        System.out.println("Information in byte form: " + Arrays.toString(Arrays.copyOfRange(packet.getData(), 0, packetLength)));
+
+    }
+
     //returns the position as integer next to the 0
-//    private int getZero(byte[] msg, int currPos)
-//    {
-//        for (int i = currPos; i < msg.length; i++){
-//            if (msg[i] == 0){
-//                return i;
-//            }
-//        }
-//        return 0;
-//    }
-      
-//    protected void makeFolder(String pathname)
-//    {
-//        File f1 = new File(pathname);
-//
-//        if(!f1.exists()){
-//            try{
-//                f1.mkdirs();
-//            }
-//            catch(SecurityException se){
-//                se.printStackTrace();
-//            }
-//        }
-//    }
-
-  //passes a string array that contains the request made by the client 
-    private String[] createArray(DatagramPacket packet) throws InvalidRequestException
+    public static int getZero(byte[] data, int length, int startPos)
     {
-    	byte[] message = packet.getData();
-        try {
-            Utils.checkPacketStructure(packet, Utils.REQ);
-        }
-        catch (Utils.InvalidPacketException e){
-            System.out.println("Invalid packet structure was found:" + e.getMessage());
-            throw new InvalidRequestException("Illegal TFTP operation.");
-//            System.exit(1);
-        }
-                  
-        String[] req = new String[3];
-        if (message[1] == 1)  {
-            req[0] = "Read";
-        } 
-        else if (message[1] == 2){
-            req[0] = "Write";
-        } 
-        Integer prev = 1;
-        Integer next =  Utils.getZero(packet.getData(), packet.getLength(), prev);
-          
-        req[1] = new String(Arrays.copyOfRange(message, prev + 1, next));
-          
-        prev = new Integer(next);
-        next =  Utils.getZero(packet.getData(), packet.getLength(), prev+1);
-          
-        req[2] = new String(Arrays.copyOfRange(message, prev + 1, next));
-          
-        return req;
-    }
-     
- 
-//    //check to see if the data packet is valid and formatted properly
-//    private void packetVerify(byte[] packetData) throws InvalidRequestException
-//    {
-//        if(packetData[0] != (byte)0){
-//            throw new InvalidRequestException("Error first byte is not a 0");
-//        }
-//        if(packetData[1] != (byte)1 && packetData[1] != (byte)2){
-//            throw new InvalidRequestException("Error second byte is not a 1 or a 2");
-//        }
-//        if(packetData[2] == (byte)0){
-//            throw new InvalidRequestException("Error no file name");
-//        }
-//    }
-      
-    //lets the client know the response depending on which type of request is being received
-    private void reqHandle(DatagramPacket packet, int port){
-    	byte[] message;
-        String[] req = {};
-          
-        try {
-            DatagramSocket requestSocket = new DatagramSocket();
-            try {
-                req = this.createArray(packet);
-            } catch (InvalidRequestException e1) {
-                cAndSendError(requestSocket, e1.getMessage(), 4, port);
-                return;
+        for (int i = startPos; i < length; i++){
+            if (data[i] == 0){
+                return i;
             }
-              
-            if (req[0] == "Write"){
-                message = new byte[]{(byte)0, (byte)4, (byte)0, (byte)0};
-                sendBytes(requestSocket, port, message);
-                getFile(requestSocket, "Server/" + req[1], SERVER);
-            } else if (req[0] == "Read"){
-                sendFile(requestSocket, port, "Server/" + req[1], SERVER);
-            }
-            listener.removeThread();
-            requestSocket.close();
-        } catch (SocketException e) {
-            e.printStackTrace();
-            System.exit(1);
+        }
+        return -1;
+    }
+
+    //
+    public static void checkPacketStructure(DatagramPacket packet, byte expectedType) throws InvalidPacketException{
+        checkPacketStructure(packet.getData(), packet.getLength(), expectedType);
+    }
+
+    public static void checkPacketStructure(byte[] data, int length, byte expectedType) throws InvalidPacketException{
+    	
+    	int currentIndex;
+    	int index;
+    	String mode;
+    	
+        if (data[0] != 0) throw new InvalidPacketException("First byte should be zero.");
+
+        String errorMessage = String.format("Was expecting %s but received %s",
+                                            upCodeTypeMap.get(expectedType),
+                                            upCodeTypeMap.get(data[1]));
+
+        if (data[1] != ERR) {
+	        if (expectedType == REQ) {
+	            if (data[1] != WRQ && data[1] != RRQ)
+	                throw new InvalidPacketException(errorMessage);
+	        }
+	        else if (data[1] != expectedType) throw new InvalidPacketException(errorMessage);
+	        switch (data[1]) {
+	            case RRQ:
+	            	currentIndex = 2;
+	                index = getZero(data, length, currentIndex);
+	                if (index == -1)
+	                    throw new InvalidPacketException("Request packet is missing the intermediate and end 0 bytes.");
+	                if (index == currentIndex)
+	                    throw new InvalidPacketException("File Name is missing from the request packet");
+	                currentIndex = index + 1;
+	                index = getZero(data, length, currentIndex);
+	                if (index == -1)
+	                    throw new InvalidPacketException("Request packet does not end in a 0");
+	                if (index == currentIndex)
+	                    throw new InvalidPacketException("Mode is missing from the request packet");
+	                mode = new String(Arrays.copyOfRange(data,currentIndex,index)).toLowerCase();
+	                if(!mode.equals("mail") && !mode.equals("netascii") && !mode.equals("octet"))
+	                    throw new InvalidPacketException("Mode is neither mail, netascii nor octet.");
+	                break;
+	            case WRQ:
+	                currentIndex = 2;
+	                index = getZero(data, length, currentIndex);
+	                if (index == -1)
+	                    throw new InvalidPacketException("Request packet is missing the intermediate and end 0 bytes.");
+	                if (index == currentIndex)
+	                    throw new InvalidPacketException("File Name is missing from the request packet");
+	                currentIndex = index + 1;
+	                index = getZero(data, length, currentIndex);
+	                if (index == -1)
+	                    throw new InvalidPacketException("Request packet does not end in a 0");
+	                if (index == currentIndex)
+	                    throw new InvalidPacketException("Mode is missing from the request packet");
+	                mode = new String(Arrays.copyOfRange(data,currentIndex,index)).toLowerCase();
+	                if(!mode.equals("mail") && !mode.equals("netascii") && !mode.equals("octet"))
+	                    throw new InvalidPacketException("Mode is neither mail, netascii nor octet.");
+	                break;
+	            case DATA:
+	                if ((length < 5) || (length > 516))
+	                    throw new InvalidPacketException("Data packet size is incorrect.");
+	                break;
+	            case ACK:
+	                if (length != 4)
+	                    throw new InvalidPacketException("Ack packet size is incorrect.");
+	                break;
+	//            case ERR:
+	//                if (length < 5)
+	//                    throw new InvalidPacketException("Error packet is too short");
+	//                if (data[2] != 0 || data[3] > 7)
+	//                    throw new InvalidPacketException("Error code in the error packet is invalid");
+	//                if (getZero(data, 4) == -1)
+	//                    throw new InvalidPacketException("Error packet does not end in a 0");
+	//                break;
+	            default:
+	                throw new InvalidPacketException("Packet type unrecognized.");
+	        }
         }
     }
-      
-    public void run() {
-        reqHandle(reqPacket, this.pnum);
-        SendSocket.close();
+
+    //used to throw invalid exceptions
+    static class InvalidPacketException extends Exception
+    {
+        public InvalidPacketException() {}
+        public InvalidPacketException(String IREText)
+        {
+            super(IREText);
+        }
     }
-    
-          
 }
